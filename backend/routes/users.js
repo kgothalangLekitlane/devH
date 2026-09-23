7c673f7e9f68635a851ed8680f0a3fdaf6cbabf9
Kgothalang Isaac Lekitlane <isaacspeedy101@gmail.com>
2026-09-23 11:33:30 +0000

    Add notification preference routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/upload");
-const { getUsers, getUserById, getAvatar, searchCandidates, updateMyProfile, recordProfileView } = require("../controllers/userController");
+const { getUsers, getUserById, getAvatar, searchCandidates, updateMyProfile, recordProfileView, getMyNotificationPreferences, updateMyNotificationPreferences } = require("../controllers/userController");
 const authenticate = require("../middleware/authMiddleware");
 
 router.get("/", authenticate, getUsers);
 router.get("/search", authenticate, searchCandidates);
 router.get("/:id/avatar", getAvatar);
+router.get("/me/notifications", authenticate, getMyNotificationPreferences);
+router.patch("/me/notifications", authenticate, updateMyNotificationPreferences);
 router.post("/:id/view", authenticate, recordProfileView);
 router.get("/:id", getUserById);
 router.put("/me", authenticate, (req, res, next) => {
